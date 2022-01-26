e4fbc8661ed33bf0352a82eb1c16e3d470c1a1e0
janhaller <haller.jan@outlook.com>
2022-01-26 17:02:21 +0100

    Revert "added Bearer"
    This reverts commit 62669f5a496b171d7b8171c20ce570cc42b92153.

--- src/main/java/com/youtubetospotify/secret.java
+++ src/main/java/com/youtubetospotify/secret.java
@@ -3,5 +3,5 @@ package com.youtubetospotify;
 public class secret {
     //fill in spotify user_id and token
     static String spotify_user_id = "";
-    static String spotify_token = "Bearer ";
+    static String spotify_token = "";
 }
